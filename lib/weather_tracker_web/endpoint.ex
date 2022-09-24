9e173da54e267f10260a9f2d5f9d30d4202a2caa
Tom Basche <tcbasche@gmail.com>
2022-09-24 14:21:27 +0300

    Tidy up the config

--- lib/weather_tracker_web/endpoint.ex
+++ lib/weather_tracker_web/endpoint.ex
@@ -10,18 +10,6 @@ defmodule WeatherTrackerWeb.Endpoint do
     signing_salt: "wpU1gp7a"
   ]
 
-  # socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :weather_tracker,
-    gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
--- mix.exs
+++ mix.exs
@@ -10,13 +10,16 @@ defmodule WeatherTracker.MixProject do
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        prod: [
+          include_executables_for: [:unix],
+          steps: [:assemble, :tar]
+        ]
+      ]
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {WeatherTracker.Application, []},
@@ -24,13 +27,9 @@ defmodule WeatherTracker.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:decimal, "~> 2.0"},
@@ -46,12 +45,6 @@ defmodule WeatherTracker.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
