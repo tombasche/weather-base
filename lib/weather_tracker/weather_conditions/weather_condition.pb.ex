705516d267cdbff5ec6dbcc3f3142076933d7df7
Tom Basche <tcbasche@gmail.com>
2022-09-27 07:59:43 +0300

    Update proto to be floats not strings

--- lib/weather_tracker/weather_conditions/weather_condition.pb.ex
+++ lib/weather_tracker/weather_conditions/weather_condition.pb.ex
@@ -3,12 +3,12 @@ defmodule WeatherConditionEvent do
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field :timestamp, 1, type: :string
-  field :altitude_m, 2, type: :string, json_name: "altitudeM"
-  field :pressure_pa, 3, type: :string, json_name: "pressurePa"
-  field :temperature_c, 4, type: :string, json_name: "temperatureC"
-  field :humidity_rh, 5, type: :string, json_name: "humidityRh"
-  field :dew_point_c, 6, type: :string, json_name: "dewPointC"
-  field :gas_resistance_ohms, 7, type: :string, json_name: "gasResistanceOhms"
+  field :altitude_m, 2, type: :float, json_name: "altitudeM"
+  field :pressure_pa, 3, type: :float, json_name: "pressurePa"
+  field :temperature_c, 4, type: :float, json_name: "temperatureC"
+  field :humidity_rh, 5, type: :float, json_name: "humidityRh"
+  field :dew_point_c, 6, type: :float, json_name: "dewPointC"
+  field :gas_resistance_ohms, 7, type: :float, json_name: "gasResistanceOhms"
 end
 
 defmodule WeatherConditionEventResponse do
@@ -23,7 +23,7 @@ defmodule WeatherConditionService.Service do
   @moduledoc false
   use GRPC.Service, name: "WeatherConditionService", protoc_gen_elixir_version: "0.11.0"
 
-  rpc(:Create, WeatherConditionEvent, WeatherConditionEventResponse)
+  rpc :Create, WeatherConditionEvent, WeatherConditionEventResponse
 end
 
 defmodule WeatherConditionService.Stub do
