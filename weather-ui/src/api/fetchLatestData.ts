1ea40a17a6aa64a8fbe5aa1fad15dd9e2e9893ad
Tom Basche <tcbasche@gmail.com>
2022-10-05 21:41:46 +0300

    Add backend url as an env var

--- weather-ui/src/api/fetchLatestData.ts
+++ weather-ui/src/api/fetchLatestData.ts
@@ -1,7 +1,9 @@
 import { WeatherCondition } from '../types';
 
 export async function fetchLatestData<T extends WeatherCondition>() {
-  return await fetch(`${process.env.WEATHER_BASE_URL}/api/weather-conditions`)
+  return await fetch(
+    `${process.env.REACT_APP_WEATHER_BASE_URL}/api/weather-conditions`,
+  )
     .then((response) => response.json())
     .then((data) => data as T);
 }
