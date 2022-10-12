0c8a77263efa5cb931098f4422c03ffbc5493f9b
Tom Basche <tcbasche@gmail.com>
2022-10-12 23:13:49 +0300

    Add air quality component

---
+++ weather-ui/src/components/AirQuality.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AirQualityRating } from '../types';
+import { airQuality } from '../utils';
+
+type Props = {
+  iaq: number;
+};
+
+const readableRating = (rating: AirQualityRating): string => {
+  switch (rating) {
+    case 'GOOD':
+      return 'good 😌';
+    case 'MODERATE':
+      return 'moderate 😐';
+    case 'UNHEALTHY_SENSITIVE':
+      return 'unhealthy for sensitive groups 👶👴';
+    case 'UNHEALTHY':
+      return 'unhealthy 🤢';
+    case 'VERY_UNHEALTHY':
+      return 'very unhealthy 🤮';
+    case 'HAZARDOUS':
+      return 'hazardous ☢️';
+  }
+};
+
+const AirQuality = ({ iaq }: Props) => {
+  return <div>Air quality is {readableRating(airQuality(iaq))}</div>;
+};
+
+export default AirQuality;
