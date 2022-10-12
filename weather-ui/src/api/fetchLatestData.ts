d2afe305c513cfe49b1acb9b73f5ed278a32fb2a
Tom Basche <tcbasche@gmail.com>
2022-10-12 08:55:28 +0300

    Hardcode in source
    We'll need to handle this nicer - not sure how we will
    display different sources as of yet, but its not realy a problem
    for right now

--- weather-ui/src/api/fetchLatestData.ts
+++ weather-ui/src/api/fetchLatestData.ts
@@ -2,7 +2,7 @@ import { WeatherCondition } from '../types';
 
 export async function fetchLatestData<T extends WeatherCondition>() {
   return await fetch(
-    `${process.env.REACT_APP_WEATHER_BASE_URL}/api/weather-conditions`,
+    `${process.env.REACT_APP_WEATHER_BASE_URL}/api/weather-conditions?source=outside`,
   )
     .then((response) => response.json())
     .then((data) => data as T);
