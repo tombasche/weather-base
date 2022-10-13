baa23fccc994c76081c451cf6dc8d3e999578401
Tom Basche <tcbasche@gmail.com>
2022-10-13 19:41:17 +0300

    Perform calculations on the backend

--- lib/weather_tracker_web/controllers/weather_condition_response.ex
+++ lib/weather_tracker_web/controllers/weather_condition_response.ex
@@ -17,15 +17,27 @@ defmodule WeatherTrackerWeb.WeatherConditionsResponse do
     :math.log(gas_resistance) + 0.04 * humidity
   end
 
+  defp decimals_to_api_format(decimal) do
+    decimal |> Decimal.to_float() |> Kernel.round()
+  end
+
+  defp float_to_api_format(float) do
+    float |> Kernel.round()
+  end
+
   def from_weather_condition(wc) do
     %{
       timestamp: wc.timestamp,
-      pressure_pa: wc.pressure_pa,
-      temperature_c: wc.temperature_c,
-      humidity_rh: wc.humidity_rh,
-      dew_point_c: wc.dew_point_c,
+      temperature_c: decimals_to_api_format(wc.temperature_c),
+      humidity_rh: decimals_to_api_format(wc.humidity_rh),
       iaq:
-        calculate_iaq(Decimal.to_float(wc.gas_resistance_ohms), Decimal.to_float(wc.humidity_rh))
+        float_to_api_format(
+          calculate_iaq(
+            Decimal.to_float(wc.gas_resistance_ohms),
+            Decimal.to_float(wc.humidity_rh)
+          )
+        ),
+      feels_like_c: 14
     }
   end
 end
---
+++ test/weather_conditions/api_format_test.exs
@@ -0,0 +1,29 @@
+defmodule WeatherTrackerWeb.WeatherConditionsApiFormatTest do
+  use WeatherTrackerWeb.ConnCase
+  use Plug.Test
+
+  test "get latest entry for source" do
+    raw_data = %{
+      timestamp: ~U[2022-09-04 04:55:17Z],
+      altitude_m: Decimal.new(0),
+      pressure_pa: Decimal.new(10_000),
+      temperature_c: Decimal.from_float(15.12312312312),
+      humidity_rh: Decimal.from_float(80.27123132123),
+      dew_point_c: Decimal.new(4),
+      gas_resistance_ohms: Decimal.new(2000),
+      source: "outside"
+    }
+
+    api_response = WeatherTrackerWeb.WeatherConditionsResponse.from_weather_condition(raw_data)
+
+    expected_response = %{
+      timestamp: ~U[2022-09-04 04:55:17Z],
+      temperature_c: 15,
+      feels_like_c: 14,
+      humidity_rh: 80,
+      iaq: 11
+    }
+
+    assert api_response == expected_response
+  end
+end
