63016913e4b5df93c5f059ec23ab98958d54f214
Tom Basche <tcbasche@gmail.com>
2022-12-27 06:44:59 +0200

    Add swish animation on click

--- weather-ui/src/components/Temperature.tsx
+++ weather-ui/src/components/Temperature.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { toUnit } from '../utils';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { TemperatureUnit } from '../types';
 
 type Props = {
@@ -8,9 +8,34 @@ type Props = {
   unit: TemperatureUnit;
 };
 
-const Root = styled.div`
+const Shimmer = keyframes`
+  100% {
+    mask-position: left;
+  }
+`;
+
+type RootProps = {
+  animate: boolean;
+};
+
+const Root = styled.div<RootProps>`
   font-size: 96px;
   filter: brightness(125%);
+  display: inline-block;
+  mask: ${(props) =>
+    props.animate
+      ? 'linear-gradient(-60deg, #000 30%, #0005, #000 70%) right/300% 100%'
+      : 'none'};
+  animation: ${Shimmer} 2.5s;
+
+  &:focus {
+    animation: ${Shimmer} 1s;
+  }
+
+  &:active {
+    animation: none;
+    mask: none;
+  }
 `;
 
 const unitIndicator = (unit: TemperatureUnit) => {
@@ -24,10 +49,21 @@ const unitIndicator = (unit: TemperatureUnit) => {
   }
 };
 
-const Temperature = ({ temperature, unit }: Props) => (
-  <Root>
-    {toUnit(temperature, unit)}°{unitIndicator(unit)}
-  </Root>
-);
+const Temperature = ({ temperature, unit }: Props) => {
+  const [animate, setAnimate] = React.useState<boolean>(true);
+
+  // Stop gradient from appearing after first animation
+  React.useEffect(() => {
+    setTimeout(() => {
+      setAnimate(false);
+    }, 2500);
+  }, []);
+
+  return (
+    <Root animate={animate} onClick={() => setAnimate(true)}>
+      {toUnit(temperature, unit)}°{unitIndicator(unit)}
+    </Root>
+  );
+};
 
 export default Temperature;
