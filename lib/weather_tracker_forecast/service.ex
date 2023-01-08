1a166ea30e06feb30aaae64e9837b8b7fa243728
Tom Basche <tcbasche@gmail.com>
2023-01-08 19:55:56 +0200

    Map the raw API response to our internal model

--- lib/weather_tracker_forecast/service.ex
+++ lib/weather_tracker_forecast/service.ex
@@ -1,10 +1,12 @@
 defmodule WeatherTrackerForecast.PredictionService do
   @moduledoc false
+
+  alias WeatherTrackerForecast.Prediction
   def client, do: Application.get_env(:weather_tracker, :client)
 
   def get_prediction(start_date, end_date) do
     case client().get_prediction(start_date, end_date) do
-      {:ok, result} -> {:ok, result}
+      {:ok, result} -> {:ok, Prediction.from_api_response(result)}
       {:error, err} -> err
     end
   end
--- test/weather_forecast/client_test.exs
+++ test/weather_forecast/client_test.exs
@@ -6,11 +6,57 @@ defmodule WeatherTrackerForecast.PredictionClientTest do
   describe "get prediction" do
     test "it gets prediction data when given start and end date" do
       PredictionClientBehaviourMock
-      # TODO match this with what we should expect to save to the DB
-      |> expect(:get_prediction, fn _start_date, _end_date -> {:ok, %{}} end)
+      |> expect(:get_prediction, fn _start_date, _end_date ->
+        {:ok,
+         %{
+           latitude: 60.16998,
+           longitude: 24.94519,
+           generationtime_ms: 8.794903755187988,
+           utc_offset_seconds: 7200,
+           timezone: "Europe/Helsinki",
+           timezone_abbreviation: "EET",
+           elevation: 7.0,
+           hourly_units: %{time: "iso8601", snowfall: "cm", rain: "mm"},
+           hourly: %{
+             time: [
+               "2023-01-05T00:00",
+               "2023-01-05T01:00",
+               "2023-01-05T02:00"
+             ],
+             snowfall: [
+               2.0,
+               1.0,
+               3.0
+             ],
+             rain: [
+               1.0,
+               0.0,
+               2.0
+             ]
+           }
+         }}
+      end)
 
       result = PredictionService.get_prediction("2022-01-01", "2022-01-02")
-      assert {:ok, %{}} == result
+
+      assert {:ok,
+              [
+                %{
+                  timestamp: ~N[2023-01-05 00:00:00.000],
+                  snow_cm: 2.0,
+                  rain_mm: 1.0
+                },
+                %{
+                  timestamp: ~N[2023-01-05 01:00:00.000],
+                  snow_cm: 1.0,
+                  rain_mm: 0.0
+                },
+                %{
+                  timestamp: ~N[2023-01-05 02:00:00.000],
+                  snow_cm: 3.0,
+                  rain_mm: 2.0
+                }
+              ]} == result
     end
   end
 end
