5bcd3f76b4b086b7dd00de1760de193d81ad387d
Tom Basche <tcbasche@gmail.com>
2023-01-08 14:59:41 +0200

    Refactor a bit

--- lib/weather_tracker_forecast/api_formats/prediction.ex
+++ lib/weather_tracker_forecast/api_formats/prediction.ex
@@ -8,25 +8,20 @@ defmodule WeatherTrackerForecast.Prediction do
   ]
 
   def from_api_response(api_response) do
-    api_response.hourly |> to_format()
+    api_response.hourly |> to_format() |> Enum.filter(fn obj -> obj.timestamp != nil end)
   end
 
   defp to_format(raw) do
-    timestamps = Enum.map(raw.time, &to_naive_dt/1) |> Enum.filter(& &1)
-    snow = raw.snowfall
-    rain = raw.rain
+    timestamps = Enum.map(raw.time, &to_naive_dt/1)
 
-    [timestamps, snow, rain]
+    [timestamps, raw.snowfall, raw.rain]
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&expand_to_object/1)
   end
 
   defp expand_to_object(values) do
-    timestamp = Enum.at(values, 0)
-    snow = Enum.at(values, 1)
-    rain = Enum.at(values, 2)
-    %{timestamp: timestamp, snow_cm: snow, rain_mm: rain}
+    %{timestamp: Enum.at(values, 0), snow_cm: Enum.at(values, 1), rain_mm: Enum.at(values, 2)}
   end
 
   defp to_naive_dt(raw_time) do
