fcde97f5df7ef21cdf240f358a18d862434a1289
Tom Basche <tcbasche@gmail.com>
2023-01-13 09:13:51 +0200

    Update predictions every hour

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,7 @@ config(:phoenix, :json_library, Jason)
 
 config :weather_tracker, client: WeatherTrackerForecast.PredictionClient
 
-# Milliseconds in 12 hours (temporarily 10 minutes for testing)
-config :weather_tracker, :forecast_fetch_interval, 600_000
+# Milliseconds in an hour
+config :weather_tracker, :forecast_fetch_interval, 3_600_000
 
 import_config("#{config_env()}.exs")
