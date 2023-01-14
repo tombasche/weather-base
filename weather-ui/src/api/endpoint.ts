46ae43e933dac10b46b64044422fd7ff00664f3c
Tom Basche <tcbasche@gmail.com>
2023-01-14 19:42:28 +0200

    Improve the prediction mocks
    This is so they're more in line (maybe) with the backend

--- weather-ui/src/api/endpoint.ts
+++ weather-ui/src/api/endpoint.ts
@@ -2,7 +2,4 @@ export const BASE_URL = `${process.env.REACT_APP_WEATHER_BASE_URL}/api`;
 export const LATEST_URL = `${BASE_URL}/weather-conditions`;
 export const AGGREGATED_URL = `${BASE_URL}/weather-conditions-aggregated`;
 
-const BASE_PREDICTION_URL = process.env.REACT_APP_PREDICTION_BASE_URL
-const helsinkiLat = 60.17
-const helsinkiLon = 24.94
-export const PREDICTION_URL = `${BASE_PREDICTION_URL}/v1/forecast?latitude=${helsinkiLat}&longitude=${helsinkiLon}&hourly=snowfall,rain&timezone=Europe/Helsinki`
+const PREDICTION_URL = process.env.REACT_APP_PREDICTION_BASE_URL;
